990f623abcce929d96b6ed9e5e6320c1f61131d1
Steve Estrada <stevee1@uci.edu>
2016-01-16 16:15:00 -0800

    Script update
    checker

--- Source/GameJam2016/Ship.cpp
+++ Source/GameJam2016/Ship.cpp
@@ -4,6 +4,7 @@
 #include "Ship.h"
 #include "PaperSpriteComponent.h"
 #include "Bounds.h"
+#include "Projectile.h"
 
 // Sets default values
 AShip::AShip()
@@ -27,6 +28,25 @@ AShip::AShip()
 	ShipSprite->bMultiBodyOverlap = true;
 	ShipSprite->GetBodyInstance()->bUseCCD = true;
 
+	ConstructorHelpers::FObjectFinder<UPaperSprite> PlayerProjectileAsset(TEXT("PaperSprite'/Game/Sprites/NormalShot.NormalShot'"));
+	ProjectilesArray.Reserve(10);
+	for (int i = 0; i < 10; i++)
+	{
+		//FString Name = "Projectile" + i;
+		UPaperSpriteComponent* PlayerProjectile = CreateDefaultSubobject<UPaperSpriteComponent>(TEXT("PlayerProjectile"+i));
+		PlayerProjectile->SetSprite(PlayerProjectileAsset.Object);
+
+		PlayerProjectile->SetCollisionObjectType(ECollisionChannel::ECC_WorldDynamic);
+		PlayerProjectile->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+		PlayerProjectile->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
+		PlayerProjectile->SetVisibility(true);
+		PlayerProjectile->SetEnableGravity(false);
+		PlayerProjectile->SetSimulatePhysics(false);
+		PlayerProjectile->SetAbsolute(true, true, true);
+
+		ProjectilesArray.Push(PlayerProjectile);
+	}
+
 	ShootingSound = CreateDefaultSubobject<UAudioComponent>(TEXT("ShootingSound"));
 	ConstructorHelpers::FObjectFinder<USoundBase> ShootingSoundAsset(TEXT("SoundWave'/Game/SFX/Burn.Burn'"));
 	ShootingSound->SetSound(ShootingSoundAsset.Object);
@@ -36,7 +56,13 @@ AShip::AShip()
 	MaxHorizontalSpeed = 500.0f;
 	CurrentVerticalSpeed = 0.0f;
 	CurrentHorizontalSpeed = 0.0f;
+
+	MaxShots = 5;
+	ShotsInUse = 0;
 	bCanMoveLeft = true;
+	bCanShoot = true;
+	ProjectileSpeed = 600.0f;
+	ProjectileRotation = FRotator(90.0f, 90.0f, 0.0f);
 
 	AutoPossessPlayer = EAutoReceiveInput::Player0;
 	DebugString = "";
@@ -46,6 +72,10 @@ AShip::AShip()
 void AShip::BeginPlay()
 {
 	ShootingSound->Stop();
+	for (int i = 0; i < ProjectilesArray.Num(); i++)
+	{
+		ProjectilesArray[i]->RelativeRotation = ProjectileRotation;
+	}
 	Super::BeginPlay();
 
 }
@@ -58,6 +88,10 @@ void AShip::Tick(float DeltaTime)
 	MakeMovements(DeltaTime);
 	ShipSprite->GetOverlappingComponents(OverlappingComponents);
 	UpdateOverlappingComponents(OverlappingComponents);
+	if (ShotsInUse > 0)
+	{
+		UpdateProjectiles(DeltaTime);
+	}
 
 	switch (ShipState)
 	{
@@ -96,6 +130,7 @@ void AShip::SetupPlayerInputComponent(class UInputComponent* InputComponent)
 	Super::SetupPlayerInputComponent(InputComponent);
 
 	InputComponent->BindAction("Fire", IE_Pressed, this, &AShip::Fire);
+	InputComponent->BindAction("Fire", IE_Released, this, &AShip::StopFire);
 	InputComponent->BindAxis("MoveUp", this, &AShip::MoveUp);
 	InputComponent->BindAxis("MoveRight", this, &AShip::MoveRight);
 }
@@ -107,7 +142,19 @@ void AShip::Fire()
 	{
 		return;
 	}
-	ShootingSound->Play();
+
+	if (ShotsInUse <= MaxShots && bCanShoot)
+	{
+		ShootProjectile();
+		ShootingSound->Play();
+		bCanShoot = false;
+	}
+}
+
+void AShip::StopFire()
+{
+	//use this because the shot gets shot multiple times with one click of the space bar
+	bCanShoot = true;
 }
 
 void AShip::MoveUp(float AxisValue)
@@ -220,6 +267,41 @@ void AShip::UpdateOverlappingComponents(TArray<UPrimitiveComponent*>& Overlappin
 			FVector TeleportLocation = FVector(OverlappingComponents[i]->GetComponentLocation().X + 1.0f,
 				GetActorLocation().Y, 0.0f); // code fore teleportation
 			SetActorLocation(TeleportLocation);
+		}
+	}
+}
+
+void AShip::ShootProjectile()
+{
+	for (int i = 0; i < ProjectilesArray.Num(); i++)
+	{
+		if (ProjectilesArray[i]->IsVisible())
+		{
+			continue;
+		}
+		else
+		{
+			FVector ProjectileLocation = FVector(GetActorLocation().X ,//+ ShipSprite->Bounds.BoxExtent.X
+				GetActorLocation().Y, 0.0f);
+
+			ProjectilesArray[i]->SetWorldLocation(ProjectileLocation);
+			ProjectilesArray[i]->SetVisibility(true);
+			ShotsInUse++;
+			return;
+		}
+	}
+}
+
+void AShip::UpdateProjectiles(float DeltaTIme)
+{
+	for (int i = 0; i < ProjectilesArray.Num(); i++)
+	{
+		if (ProjectilesArray[i]->IsVisible())
+		{
+			FVector NewProjectileLocation = FVector(ProjectilesArray[i]->GetComponentLocation().X + ProjectileSpeed*DeltaTIme,
+				ProjectilesArray[i]->GetComponentLocation().Y, 0.0f);
+
+			ProjectilesArray[i]->SetRelativeLocation(NewProjectileLocation);
 		}
 	}
 }
